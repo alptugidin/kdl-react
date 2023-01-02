563a7a5ba084a7118ec0e13a6b0d1333c22694e8
Alptuğ <alptugidin@gmail.com>
2023-01-02 05:41:51 +0300

    Clear svg component

---
+++ components/SVG/Clear.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Clear: React.FC = () => {
+  return (
+
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M3 3L17 17M3 17L17 3L3 17Z" stroke="#9AA0A6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+
+  );
+};
+
+export default Clear;
