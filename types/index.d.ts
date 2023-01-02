772d42b3f6b8c97ac3f1bf9710ee2d971635f091
Alptuğ <alptugidin@gmail.com>
2023-01-02 05:44:40 +0300

    interface for Series

---
+++ types/index.d.ts
@@ -0,0 +1,12 @@
+export interface ISeries {
+   idx: string
+   name: string
+   year: string
+   tags: []
+   aka: []
+   co: number
+   summary: string
+   summaryLink: string
+   video: string
+   title: string
+ }
