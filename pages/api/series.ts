b8a1b83c383d3483e74d363501c417f9dde3050e
Alptuğ <alptugidin@gmail.com>
2023-01-02 05:44:18 +0300

    series handler

---
+++ pages/api/series.ts
@@ -0,0 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import dbConnect from '../../util/db';
+import Serie from './models/series';
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await dbConnect();
+  const series = await Serie.find({name: {$regex: req.query.q, $options: "i"}}).limit(10);
+  res.send(series);
+}
