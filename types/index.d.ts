08d7052c4f3b7b819c21f40651596fe054175db4
Alptuğ <alptugidin@gmail.com>
2023-01-03 05:52:47 +0300

    new interfaces

--- types/index.d.ts
+++ types/index.d.ts
@@ -10,3 +10,30 @@ export interface ISeries {
    video: string
    title: string
  }
+
+export interface ITags {
+  tag: string
+ }
+
+
+export interface ISearchResults {
+  resultRef: React.RefObject<HTMLUListElement>
+  results: {
+    series: ISeries[];
+    tags: ITags[];
+  }
+  setResults: React.Dispatch<React.SetStateAction<{
+    series: ISeries[];
+    tags: ITags[];
+  }>>
+   searchBox: {
+    value: string;
+    searchType: string;
+    placeholder: string;
+  }
+  setSearchBox: React.Dispatch<React.SetStateAction<{
+    value: string;
+    searchType: string;
+    placeholder: string;
+  }>>
+ }
