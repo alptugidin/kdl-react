55189174885ce8b34cdea4bb37649c8ed42e58b6
Alptuğ <alptugidin@gmail.com>
2023-01-08 03:03:54 +0300

    update

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,39 +1,43 @@
+import React from 'react';
+
 export interface ISeries {
-   idx: string
-   name: string
-   year: string
-   tags: []
-   aka: []
-   co: number
-   summary: string
-   summaryLink: string
-   video: string
-   title: string
- }
+    idx: string
+    name: string
+    year: string
+    tags: []
+    aka: []
+    co: number
+    summary: string
+    summaryLink: string
+    video: string
+    title: string
+}
 
 export interface ITags {
-  tag: string
- }
-
+    tag: string
+}
 
 export interface ISearchResults {
-  resultRef: React.RefObject<HTMLUListElement>
-  results: {
-    series: ISeries[];
-    tags: ITags[];
-  }
-  setResults: React.Dispatch<React.SetStateAction<{
-    series: ISeries[];
-    tags: ITags[];
-  }>>
-   searchBox: {
-    value: string;
-    searchType: string;
-    placeholder: string;
-  }
-  setSearchBox: React.Dispatch<React.SetStateAction<{
-    value: string;
-    searchType: string;
-    placeholder: string;
-  }>>
- }
+    resultRef: React.RefObject<HTMLUListElement>
+    inputRef: React.RefObject<HTMLInputElement>
+    results: {
+        series: ISeries[];
+        tags: ITags[];
+    }
+    setResults: React.Dispatch<React.SetStateAction<{
+        series: ISeries[];
+        tags: ITags[];
+    }>>
+    searchBox: {
+        ref: React.RefObject<HTMLDivElement>;
+        value: string;
+        searchType: string;
+        placeholder: string;
+    }
+    setSearchBox: React.Dispatch<React.SetStateAction<{
+        ref: React.RefObject<HTMLDivElement>;
+        value: string;
+        searchType: string;
+        placeholder: string;
+    }>>
+}
