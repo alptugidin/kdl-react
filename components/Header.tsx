bcb758c3ddd5ba938eca302c39c0f6df376eeeb9
Alptuğ <alptugidin@gmail.com>
2023-01-15 01:42:51 +0300

    make responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,16 +7,16 @@ const Header: React.FC = () => {
 
   const router = useRouter();
   return (
-    <div className='h-16 w-full bg-gradient-to-r from-rose-400 to-blue-400 shadow-sm border-b absolute left-0 right-0'>
-      <div className='container mx-auto flex h-full'>
-        <div className='basis-1/3 flex items-center'>
+    <div className='md:h-16 h-20 w-full bg-gradient-to-r from-rose-400 to-blue-400 shadow-sm border-b absolute left-0 right-0'>
+      <div className='container mx-auto md:flex h-full'>
+        <div className='basis-1/3 flex items-center md:justify-start justify-center md:h-auto h-10'>
           <Image
             src='/logoSVG.svg'
-            width={200} height={0} alt='logo'
-            className='cursor-pointer select-none'
+            width={0} height={0} alt='logo'
+            className='cursor-pointer select-none md:w-[200px] w-[120px]'
             onClick={() => router.push('/') } />
         </div>
-        <div className='basis-1/3 flex items-center'>
+        <div className='md:w-[550px] flex items-center md:h-auto h-10 md:px-auto px-0.5'>
           <SearchBox/>
         </div>
         <div className='basis-1/3 flex items-center'></div>
