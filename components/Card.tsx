f50e7855baef25b00cc1ed9255752bf0d26faed4
Alptuğ <alptugidin@gmail.com>
2023-01-16 01:14:51 +0300

    new Card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import Image from 'next/image';
+import { Flat } from '@alptugidin/react-circular-progress-bar';
 
 const Card: React.FC = () => {
 
   return (
-    <div className='w-44 h-56 bg-yellow-100 rounded-2xl border-blue-500 border card'>
-
+    <div className='card group cursor-pointer relative'>
+      <div className='w-16 h-16 absolute right-0 top-0 z-50'>
+        <Flat
+          progress={50}
+          text={'Match'}
+          showMiniCircle={ false }
+          sx={{
+            barColor: '#ff0000',
+            barWidth: 5,
+            bgColor: '#d9d9d9',
+            strokeLinecap: 'butt',
+            valueSize: 33,
+            textSize: 29
+          }}
+        />
+      </div>
+      <div className='w-fit transition-all overflow-hidden group-hover:scale-110 duration-[400ms]'>
+        <Image src='/img/136.webp' width={160} height={120} alt='poster'/>
+      </div>
     </div>
   );
-
 };
 
 export default Card;
