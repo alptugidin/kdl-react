9cf216ff8ca549a2190c6a8ffd42ce85144e3103
Alptuğ <alptugidin@gmail.com>
2023-01-21 00:55:15 +0300

    update indent

--- components/CardsContainer.tsx
+++ components/CardsContainer.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import Card from './Card';
-export const CardsContainer: React.FC<{  }> = props => {
-  // @ts-ignore
-  return (
-    <div className='container mx-auto flex justify-center mt-10 flex-col gap-5'>
-      <span className='font-ubuntu text-3xl text-gray-600 text-center'>Latest Korean Dramas</span>
-      <div className='w-full h-56 border rounded-lg p-1 flex gap-1 overflow-hidden'>
-        {Array.from(Array(20).keys()).map((i,_) => (
-          <div key={_}>
-            <Card/>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+
+export const CardsContainer: React.FC<{}> = props => {
+    return (
+        <div className='container mx-auto flex justify-center mt-10 flex-col gap-5 cards-container'>
+            <span className='font-ubuntu text-3xl text-gray-600 text-center'>Latest Korean Dramas</span>
+            <div className='w-full border rounded-lg p-1 flex gap-1 sm:overflow-hidden overflow-auto'>
+                {Array.from(Array(20).keys()).map((i, _) => (
+                    <div key={_}>
+                        <Card/>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 
