2cf7c8db6af438e20a5dc49f9e7c7752b68d7baa
Alptuğ <alptugidin@gmail.com>
2023-01-21 00:54:56 +0300

    update progress bar styling

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,31 +1,36 @@
 import React from 'react';
 import Image from 'next/image';
-import { Flat } from '@alptugidin/react-circular-progress-bar';
+import {Flat} from '@alptugidin/react-circular-progress-bar';
 
 const Card: React.FC = () => {
 
-  return (
-    <div className='card group cursor-pointer relative'>
-      <div className='w-16 h-16 absolute right-0 top-0 z-50'>
-        <Flat
-          progress={50}
-          text={'Match'}
-          showMiniCircle={ false }
-          sx={{
-            barColor: '#ff0000',
-            barWidth: 5,
-            bgColor: '#d9d9d9',
-            strokeLinecap: 'butt',
-            valueSize: 33,
-            textSize: 29
-          }}
-        />
-      </div>
-      <div className='w-fit transition-all overflow-hidden group-hover:scale-110 duration-[400ms]'>
-        <Image src='/img/136.webp' width={160} height={120} alt='poster'/>
-      </div>
-    </div>
-  );
+    return (
+        <div className='card group cursor-pointer relative'>
+            <div className='lg:w-16 lg:h-16 h-11 w-11 absolute right-0 top-0 z-50'>
+                <Flat
+                    progress={50}
+                    text={'Match'}
+                    showMiniCircle={false}
+                    sx={{
+                        strokeColor: '#ff0000',
+                        barWidth: 5,
+                        bgStrokeColor: '#d9d9d9',
+                        strokeLinecap: 'butt',
+                        bgColor: {value: '#000000', transparency: '70'},
+                        valueSize: 32,
+                        textSize: 28,
+                        textColor: '#facc15',
+                        valueColor: '#facc15',
+                        valueWeight: 'bold',
+                        textWeight: 'bold'
+                    }}
+                />
+            </div>
+            <div className='w-fit transition-all overflow-hidden group-hover:scale-110 duration-[400ms]'>
+                <Image src='/img/136.webp' width={160} height={120} alt='poster'/>
+            </div>
+        </div>
+    );
 };
 
 export default Card;
