3712c6e1649f67f68c2c3cfcdd15f19e16de1b8a
Alptuğ <alptugidin@gmail.com>
2023-01-22 23:14:15 +0300

    fix imports and exports

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,7 @@
-import Header from "./Header";
-import SearchBox from "./SearchBox/SearchBox";
-export {Header, SearchBox};
+import Header from './Header';
+import SearchBox from './SearchBox/SearchBox';
+import LatestContainer from './LatestContainer';
+import PopularsContainer from './PopularsContainer';
+import Chevron from './Chevron';
+import Card from './Card';
+export {Header, SearchBox, LatestContainer, PopularsContainer, Chevron,Card};
