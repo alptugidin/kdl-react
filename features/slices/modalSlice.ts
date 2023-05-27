28cda293db6f0221c21b68ba55acd0486951a605
Alptuğ <alptugidin@gmail.com>
2023-05-28 01:49:43 +0300

    add closeModal function

--- features/slices/modalSlice.ts
+++ features/slices/modalSlice.ts
@@ -1,22 +1,29 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {RootState} from '../../store/store';
+import {ISeries} from '../../types';
 
 interface IModal {
   isOpen: boolean;
-  content: {};
+  content: ISeries | null;
 }
 
 const initialState: IModal = {
   isOpen: false,
-  content: {},
+  content: null,
 };
 
 const modalSlice = createSlice({
   name: 'modal',
   initialState,
   reducers: {
-    openModal: (state, action: PayloadAction<boolean>) => {
-      state.isOpen = action.payload;
+    openModal: (state, action: PayloadAction<IModal>) => {
+      state.isOpen = action.payload.isOpen;
+      state.content = action.payload.content;
+    },
+
+    closeModal: (state) => {
+      state.isOpen = false;
+      state.content = null;
     }
   }
 });
